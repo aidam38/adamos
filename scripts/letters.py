dee72328e6755538cf3ef4dfa5d5d93e6dae13c9
Adam Krivka <krivka.adam@gmail.com>
2019-02-10 21:18:59 +0100

    Minor changes

---
+++ scripts/letters.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import json
+import operator
+from pprint import pprint
+
+with open('/home/adam/letters.json') as f:
+    data = json.load(f)
+
+data_iter = []
+for key in data:
+    tup = (key, data[key])
+    data_iter.append(tup)
+
+data_sorted = sorted(data_iter, key=lambda x: x[1], reverse=True)
+
+
+for i in range(len(data_sorted)):
+    print(str(i) + " " + data_sorted[i][0] + " " + str(data_sorted[i][1]))
