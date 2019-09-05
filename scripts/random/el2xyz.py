e033f15b981b8f5a05d1c50b3bff514b34c60fa2
Adam Křivka <krivka.adam@gmail.com>
2019-09-05 19:19:25 +0200

    kakrc upgrades

--- scripts/random/el2xyz.py
+++ scripts/random/el2xyz.py
@@ -4,8 +4,8 @@
 __author__ = "Adam Krivka"
 
 import sys
-import os
-from math import *
+from math import sin, cos, tan, atan, radians, sqrt
+
 
 def kepler(M, e):
     """Solution of Kepler equation"""
@@ -15,39 +15,44 @@ def kepler(M, e):
         E1 = E2
         E2 = M + e*sin(E1)
         if abs(E2 - E1) < eps:
-            break 
+            break
     return E2
 
+
 def el2xyz(path):
-    """Conversion from orbital elements to xyz positions (from text file bin.out by program follow2)"""
+    """Conversion from orbital elements to xyz positions
+    (from text file bin.out by program follow2)"""
     binout = open(path, 'r')
     for line in binout.readlines():
-        l = line.split()
-        id = int(l[0])
+        els = line.split()
+        id = int(els[0])
         if id > 0:
-            t = float(l[1])
-            a = float(l[2])
-            e = float(l[3])
-            inc = radians(float(l[4]))
-            capom = radians(float(l[5]))
-            omega = radians(float(l[6]))
-            M = radians(float(l[7]))
-    
-            # Note: The approximation by 3 terms was not sufficient
-            # E = M + (e-pow(e,3.0)/8.0)*sin(M) + pow(e,2)/2.0*sin(2.0*M) + pow(e,3)*3.0/8.0*sin(3.0*M)
+            t = float(els[1])
+            a = float(els[2])
+            e = float(els[3])
+            inc = radians(float(els[4]))
+            capom = radians(float(els[5]))
+            omega = radians(float(els[6]))
+            M = radians(float(els[7]))
+
+            """ Note: The approximation by 3 terms was not sufficient
+             E = M + (e-pow(e,3.0)/8.0)*sin(M) +
+             pow(e,2)/2.0*sin(2.0*M) + pow(e,3)*3.0/8.0*sin(3.0*M) """
             E = kepler(M, e)
             f = 2.0*atan(sqrt((1.0+e)/(1.0-e))*tan(E/2.0))
             r = a*(1-e*cos(E))
             x = r*(cos(capom)*cos(omega+f) - sin(capom)*sin(omega+f)*cos(inc))
             y = r*(sin(capom)*cos(omega+f) + cos(capom)*sin(omega+f)*cos(inc))
             z = r*sin(inc)*sin(omega+f)
-            print (t + " " + str(x) + " " + str(y) + " " + str(z))
+            print(t + " " + str(x) + " " + str(y) + " " + str(z))
+
 
 def main():
     if (len(sys.argv) >= 2):
         el2xyz(sys.argv[1])
     else:
-        print "Usage el2xyz.py infile"
+        print("Usage el2xyz.py infile")
+
 
 if __name__ == "__main__":
     main()
